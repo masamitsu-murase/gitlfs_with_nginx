182cdea97a901f74409df89daa168bf7569ab95d
Masamitsu MURASE <masamitsu.murase@gmail.com>
2020-07-28 22:46:38 +0900

    Add MAX_FILE_SIZE to env.list, so that the user can change the maximum size of LFS file.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
 set -e
 
-envsubst '$LFS_ROOT $EXTERNAL_PORT $FLASK_PORT' < nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$LFS_ROOT $EXTERNAL_PORT $FLASK_PORT $MAX_FILE_SIZE' < nginx.conf.template > /etc/nginx/nginx.conf
 nginx -t
 nginx
 
